2b8e0e261e52211cbf4d11b08a87066a281164ce
Sherif Atitebi <sherifatitebi@gmail.com>
2022-06-24 01:41:27 +0100

    edit pdf scrapping file

--- ScrappingPDF.py
+++ ScrappingPDF.py
@@ -1,4 +1,10 @@
+# install tk
+# install ghostscript
+# install camelot-py
 import camelot
 
 tables = camelot.read_pdf("table_pdf.pdf", pages="1")
 print(tables)
+
+tables.export("pdf_table.pdf", f="csv", compress=True)
+tables[0].to_csv("pdf_table.csv")
